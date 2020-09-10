1684dd2d0d0268998e42b88e65c1492c65ca06a8
iacopo ciao <iacopociao1992@gmail.com>
2020-09-11 00:18:43 +0200

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
